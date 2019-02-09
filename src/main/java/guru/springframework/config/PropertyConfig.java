29de9ddad1fbc77073cac41342cfaa6303bbef6d
Tomasz Skrzypek <tomas14@uia.no>
2019-02-09 17:16:42 +0100

    Properties examples

---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,54 @@
+package guru.springframework.config;
+
+import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class PropertyConfig {
+
+    @Autowired
+    Environment env;
+
+    @Value("${guru.username}")
+    String user;
+
+    @Value("${guru.password}")
+    String password;
+
+    @Value("${guru.dburl}")
+    String url;
+
+    @Value("${guru.jms.username}")
+    String jmsUsername;
+
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+
+    @Value("${guru.jms.url}")
+    String jmsUrl;
+
+
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker(){
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUsername(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+        return fakeJmsBroker;
+    }
+}
